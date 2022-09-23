97fb9e9b6fad2fd487c85a6a716b8c3ca84fed0d
Макс Титов <hhit934@gmail.com>
2022-09-23 20:37:37 +0300

    Добавьте файлы проекта.

---
+++ WindowMove/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WindowMove
+{
+    internal class Program
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+            public override string ToString()
+            {
+                return $"{Left} {Top} {Right - Left} {Bottom - Top}";
+            }
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            int margin_ = 20;
+            double p = 0;
+            double speed = 3;
+            RECT rect_from;
+
+            IntPtr window_h = new IntPtr(13896);
+
+            double size_w,size_h = 0;
+
+            while (true)
+            {
+                GetWindowRect(window_h, out rect_from);
+                if (Keyboard.IsKeyDown(System.Windows.Input.Key.E))
+                {
+                    window_h = GetForegroundWindow();
+                    Console.WriteLine(window_h);
+                }
+                if (Keyboard.IsKeyDown(System.Windows.Input.Key.Space))
+                {
+                    p += speed;
+                    size_w = (rect_from.Right - rect_from.Left);
+                    Console.WriteLine(rect_from);
+
+                    if(rect_from.Left + size_w > 1920- margin_)
+                    {
+                        p = 0;
+                    }
+                   
+                  
+
+
+                }
+                else
+                {
+                    p = 0;
+                }
+
+                if(window_h != IntPtr.Zero)
+                {
+                    MoveWindow(window_h, margin_ + (int)p, margin_,
+                      rect_from.Right - rect_from.Left,
+                      rect_from.Bottom - rect_from.Top, true);
+                }
+                
+
+                Thread.Sleep(1);
+               
+            }
+        }
+    }
+}
---
+++ design/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace design
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            CoreApplicationViewTitleBar titleBar = CoreApplication.GetCurrentView().TitleBar;
+
+            ApplicationViewTitleBar appTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            titleBar.ExtendViewIntoTitleBar = true;
+
+            appTitleBar.BackgroundColor = Colors.Transparent;
+            appTitleBar.ButtonBackgroundColor = Windows.UI.Colors.Transparent;
+            appTitleBar.BackgroundColor = Windows.UI.Colors.Transparent;
+
+            
+
+
+            var _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += (o,e) =>
+            {
+
+            };
+            _dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+
+            _dispatcherTimer.Start();
+            
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Width = this.MinWidth;
+            this.Height = this.MinWidth;
+        }
+    }
+}
