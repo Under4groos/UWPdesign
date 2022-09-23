5abd900507afb425cafa62a4b8620a1ce8958f06
Макс Титов <hhit934@gmail.com>
2022-09-23 20:57:52 +0300

    dasd

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, PreserveSig = false)]
+        internal static extern void DwmSetWindowAttribute(IntPtr hwnd,
+                                                       DWMWINDOWATTRIBUTE attribute,
+                                                       ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute,
+                                                       uint cbAttribute);
+        public enum DWMWINDOWATTRIBUTE
+        {
+            DWMWA_WINDOW_CORNER_PREFERENCE = 33
+        }
+
+ 
+        public enum DWM_WINDOW_CORNER_PREFERENCE
+        {
+            DWMWCP_DEFAULT = 0,
+            DWMWCP_DONOTROUND = 1,
+            DWMWCP_ROUND = 2,
+            DWMWCP_ROUNDSMALL = 3
+        }
+ 
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.PreviewMouseLeftButtonDown += (o, e) =>
+            {
+                this.DragMove();
+            };
+
+            this.Loaded += (o, e) =>
+            {
+                //new WindowBlureffect(this, WindowBlureffect.AccentState.ACCENT_ENABLE_ACRYLICBLURBEHIND);
+
+                new WindowBlureffect(this, WindowBlureffect.AccentState.ACCENT_ENABLE_BLURBEHIND);
+
+
+                var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
+
+                DwmSetWindowAttribute(new System.Windows.Interop.WindowInteropHelper(this).Handle,
+                     DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE,
+                      ref preference,
+                      sizeof(uint)
+                    );
+            };
+        }
+    }
+}
---
+++ WpfApp/WindowBlureffect.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+public class WindowBlureffect
+{
+    // https://github.com/jdscodelab/LoginUIBlurredAcrylicBackground
+    [DllImport("user32.dll")]
+    static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+    public enum AccentState
+    {
+        ACCENT_DISABLED = 0,
+        ACCENT_ENABLE_GRADIENT = 1,
+        ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+        ACCENT_ENABLE_BLURBEHIND = 3,
+        ACCENT_ENABLE_ACRYLICBLURBEHIND = 4,
+        ACCENT_INVALID_STATE = 5
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct AccentPolicy
+    {
+        public AccentState AccentState;
+        public uint AccentFlags;
+        public uint GradientColor;
+        public uint AnimationId;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WindowCompositionAttributeData
+    {
+        public WindowCompositionAttribute Attribute;
+        public IntPtr Data;
+        public int SizeOfData;
+    }
+    public enum WindowCompositionAttribute
+    {
+        WCA_ACCENT_POLICY = 19
+    }
+    public WindowBlureffect(Window window, AccentState accentState)
+    {
+        this.window = window;
+        this.accentState = accentState;
+        EnableBlur();
+    }
+
+    #region region
+    private uint _blurOpacity;
+
+    private Window window { get; set; }
+    private AccentState accentState { get; set; }
+    public double BlurOpacity
+    {
+        get { return _blurOpacity; }
+        set { _blurOpacity = (uint)value; EnableBlur(); }
+    }
+    #endregion
+    private void EnableBlur()
+    {
+        var windowHelper = new System.Windows.Interop.WindowInteropHelper(window);
+        var accent = new AccentPolicy();
+        accent.AccentState = accentState;
+        accent.GradientColor = 0xFFFFFF;
+        var accentStructSize = Marshal.SizeOf(accent);
+        var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+        Marshal.StructureToPtr(accent, accentPtr, false);
+        var data = new WindowCompositionAttributeData();
+        data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+        data.SizeOfData = accentStructSize;
+        data.Data = accentPtr;
+        SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+        Marshal.FreeHGlobal(accentPtr);
+    }
+}
